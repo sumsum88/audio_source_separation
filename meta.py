21155111c7fb3b23ca056d2cdd4dea47298b8912
sumsum88 <hay88.sum10ms@gmail.com>
2018-03-15 23:47:10 +0900

    add drum/vocal

--- meta.py
+++ meta.py
@@ -1,13 +1,13 @@
 
-def make_html(song_name, num):
+def make_html(song_name, num, inst='bass', dir='vb'):
     scripts = '''
     <b>入力信号 "{song_name}" (DSD100 0{num})</b><br>
 
     <table>
         <tr>
             <td width=400>
-                    入力信号<br>(Bass, Vocals)<br>
-    <audio src="./src/mix_3{num}.mp3" preload="auto"></audio>
+                    入力信号<br>({Inst}, Vocals)<br>
+    <audio src="./src/{dir}/mix_3{num}.mp3" preload="auto"></audio>
             </td>
         </tr>
     </table>
@@ -16,11 +16,11 @@ def make_html(song_name, num):
     <b>Duong+DNNによる分離信号</b><br>
     <table>
         <tr>
-            <td>分離信号 1<br>(Bass)<br>
-    <audio src="./src/bass_3{num}_duongdnn.mp3" preload="auto"></audio>
+            <td>分離信号 1<br>({Inst})<br>
+    <audio src="./src/{dir}/{inst}_3{num}_duongdnn.mp3" preload="none"></audio>
             </td>
             <td>分離信号 2<br>(Vocals)<br>
-    <audio src="./src/vocals_3{num}_duongdnn.mp3" preload="auto"></audio>
+    <audio src="./src/{dir}/vocals_3{num}_duongdnn.mp3" preload="none"></audio>
             </td>
         </tr>
     </table>
@@ -30,11 +30,11 @@ def make_html(song_name, num):
     <b>DNN+WFによる分離信号</b><br>
     <table>
         <tr>
-            <td>分離信号 1<br>(Bass)<br>
-    <audio src="./src/3{num}_bass_wf.mp3" preload="auto"></audio>
+            <td>分離信号 1<br>({Inst})<br>
+    <audio src="./src/{dir}/3{num}_{inst}_wf.mp3" preload="none"></audio>
             </td>
             <td>分離信号 2<br>(Vocals)<br>
-    <audio src="./src/3{num}_vocals_wf.mp3" preload="auto"></audio>
+    <audio src="./src/{dir}/3{num}_vocals_wf.mp3" preload="none"></audio>
             </td>
         </tr>
     </table>
@@ -44,11 +44,11 @@ def make_html(song_name, num):
     <b>ILRMAによる分離信号</b><br>
     <table>
         <tr>
-            <td>分離信号 1<br>(Bass)<br>
-    <audio src="./src/bass_3{num}_ILRMA.mp3" preload="auto"></audio>
+            <td>分離信号 1<br>({Inst})<br>
+    <audio src="./src/{dir}/{inst}_3{num}_ILRMA.mp3" preload="none"></audio>
             </td>
             <td>分離信号 2<br>(Vocals)<br>
-    <audio src="./src/vocals_3{num}_ILRMA.mp3" preload="auto"></audio>
+    <audio src="./src/{dir}/vocals_3{num}_ILRMA.mp3" preload="none"></audio>
             </td>
         </tr>
     </table>
@@ -58,18 +58,18 @@ def make_html(song_name, num):
     <b>IDLMAによる分離信号</b><br>
     <table>
         <tr>
-            <td>分離信号 1<br>(Bass)<br>
-    <audio src="./src/bass_3{num}_idlma.mp3" preload="auto"></audio>
+            <td>分離信号 1<br>({Inst})<br>
+    <audio src="./src/{dir}/{inst}_3{num}_idlma.mp3" preload="none"></audio>
             </td>
             <td>分離信号 2<br>(Vocals)<br>
-    <audio src="./src/vocals_3{num}_idlma.mp3" preload="auto"></audio>
+    <audio src="./src/{dir}/vocals_3{num}_idlma.mp3" preload="none"></audio>
             </td>
         </tr>
     </table>
 
     <br>
     <div style="border:1px solid;margin:5px;"></div>
-    '''.format(song_name=song_name, num='{:02d}'.format(num))
+    '''.format(song_name=song_name, num='{:02d}'.format(num), inst=inst, Inst=inst[0].upper()+inst[1:], dir=dir)
     print(scripts)
 
 
@@ -78,3 +78,9 @@ make_html('ANiMAL - Rockshow', 2)
 make_html('Actions - One Minute Smile', 3)
 make_html('Al James - Schoolboy Facination', 4)
 make_html('Angela Thomas Wade - Milk Cow Blues', 5)
+
+make_html('ANiMAL - Clinic A', 1, inst='drums', dir='vd')
+make_html('ANiMAL - Rockshow', 2, inst='drums', dir='vd')
+make_html('Actions - One Minute Smile', 3, inst='drums', dir='vd')
+make_html('Al James - Schoolboy Facination', 4, inst='drums', dir='vd')
+make_html('Angela Thomas Wade - Milk Cow Blues', 5, inst='drums', dir='vd')
